26ad0d63eeead007b8d427435b38467df15aa27d
myh2910 <minleeyohan@gmail.com>
2021-11-01 12:17:52 -0500

    쓰기 귀찮음

--- playlist/utils.py
+++ playlist/utils.py
@@ -18,15 +18,15 @@ def lst_method(lines: list[str], method: str):
 	elif method == 'name':
 		lines = sorted(lines)
 	# TODO: artist 순대로 정렬
-	elif method == 'artist':
-		pass
+	# elif method == 'artist':
+	#
 	else:
 		print(f"{ERROR} 옵션 {INPUT}'{method}'{RESET}이 존재하지 않습니다.")
 		return False
 	return lines
 
-def lst_create(playlist: str, method: str = 'name', exts: list = ['mp3', 'mp4'], home: str = HOME):
-	"""플레이리스트 파일 `playlist`.m3u 생성.
+def lst_create(playlist: str, out = 'm3u', folder = '', method: str = 'name', exts: list = ['mp3', 'mp4'], home: str = HOME):
+	"""플레이리스트 파일 생성.
 
 	옵션 설명:
 	* `playlist`:  플레이리스트 이름.
@@ -34,22 +34,32 @@ def lst_create(playlist: str, method: str = 'name', exts: list = ['mp3', 'mp4'],
 	* `exts`: 파일 확장자들을 포함한 리스트.
 	* `home`: 플레이리스트를 포함하고 있는 폴더 이름.
 	"""
+	home += folder
 	init()
-	if os.path.exists(f'{home}/{playlist}'):
-		lst = []
-		for ext in exts:
-			lst.extend(glob.glob(f'{home}/{playlist}/*.{ext}'))
-		lst = lst_method(lst, method)
-		if lst:
-			if lst == []:
-				print(f'{ERROR} 플레이리스트 폴더 {FILE}{playlist}{RESET}가 비어 있습니다.')
-			else:
-				with open(f'{home}/{playlist}.m3u', 'w', encoding='utf8') as m3u:
-					for file in lst:
-						m3u.write(os.path.relpath(file, home) + '\n')
-				print(f'{FINISHED} 플레이리스트 생성이 완료되었습니다.')
-	else:
-		print(f'{ERROR} 플레이리스트 폴더 {FILE}{playlist}{RESET}를 찾을 수 없습니다.')
+	if out == 'm3u':
+		if os.path.exists(f'{home}/{playlist}'):
+			lst = []
+			for ext in exts:
+				lst.extend(glob.glob(f'{home}/{playlist}/*.{ext}'))
+			lst = lst_method(lst, method)
+			if lst:
+				if lst == []:
+					print(f'{ERROR} 플레이리스트 폴더 {FILE}{playlist}{RESET}가 비어 있습니다.')
+				else:
+					with open(f'{home}/{playlist}.m3u', 'w', encoding='utf8') as m3u:
+						for file in lst:
+							m3u.write(os.path.relpath(file, home) + '\n')
+					print(f'{FINISHED} 플레이리스트 생성이 완료되었습니다.')
+		else:
+			print(f'{ERROR} 플레이리스트 폴더 {FILE}{playlist}{RESET}를 찾을 수 없습니다.')
+	elif out == 'smpl':
+		with open(f'{home}/{playlist}.m3u', 'r', encoding = 'utf8') as m3u:
+			music = m3u.readlines()
+			with open(f'{home}/{playlist}.smpl', 'w', encoding = 'utf8') as smpl:
+				smpl.write("{'members': [")
+				for i, mp3 in enumerate(music):
+					smpl.write("\n\t{'info': '/storage/emulated/0/Music" + folder + '/' + mp3 + f"', 'order': {i + 1}, 'type': 65537" + "},")
+				smpl.write("\n], 'sortBy': 4}")
 
 def lst_order(playlist: str, method: str = 'suffle', home: str = HOME):
 	"""플레이리스트 정렬 기능.
--- playlist/youtube.py
+++ playlist/youtube.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 import youtube_dl
 from youtube_dl.utils import sanitize_filename
+from youtube_dl.utils import ExtractorError, DownloadError
 import glob
 import os
 from timeit import default_timer as timer
@@ -25,9 +26,11 @@ def download(
 	end: int = None,
 	playlist_name: str = None,
 	write_playlist: bool = True,
+	export_to_smpl: bool = False,
 	home: str = HOME,
+	folder: str = "",
 	write_thumbnail: bool = True,
-	retries: int = 2,
+	retries: int = 3,
 	fragment_retries: int = 3,
 	auto: bool = False
 ) -> float:
@@ -46,6 +49,8 @@ def download(
 	* `fragment_retries`: 오류 발생 시 영상 fragment 다운로드를 반복할 최대 횟수.
 	* `auto`: 다운로드 자동화.
 	"""
+	elapsed_time = -timer()
+	home += folder
 	init()
 	ydl_opts = {
 		"writethumbnail": write_thumbnail,
@@ -73,21 +78,22 @@ def download(
 		]
 	else:
 		print(f"{ERROR} 파일 확장자 {INPUT}\"{codec}\"{RESET}는 지원되지 않습니다.")
-		return 0
+		return elapsed_time + timer()
 
 	print(f"{DOWNLOAD} 웹페이지 정보 추출 중...")
-	elapsed_time = -timer()
 	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
 		i = retries
 		while True:
-			if i == 0:
-				print(f"{ERROR} 웹페이지 추출이 불가능합니다.")
-				return 0
 			try:
 				playlist_dict = ydl.extract_info(url, False)
 				break
+			except (ExtractorError, DownloadError):
+				return elapsed_time + timer()
 			except:
-				print(f"{WARNING} 에러 발견. 다시 시도 중...")
+				if i == 0:
+					print(f"{ERROR} 웹페이지 추출이 불가능합니다.")
+					return elapsed_time + timer()
+				print(f"{WARNING} 에러 발생. 다시 시도 중...")
 				ydl.cache.remove()
 				i -= 1
 		try:
@@ -119,21 +125,43 @@ def download(
 
 	if playlist != "NA" and write_playlist:
 		playlist_file = f"{home}/{playlist}.m3u"
+		message = f"플레이리스트 파일 {FILE}{playlist}.m3u{RESET}"
 		if os.path.exists(playlist_file):
 			if auto:
-				print(f"{CREATE} 플레이리스트 파일 {FILE}{playlist}.m3u{RESET} 업데이트 중...")
+				print(f"{CREATE} {message} 업데이트 중...")
 				confirm = "y"
 			else:
-				print(f"{UPDATE} 플레이리스트 파일 {FILE}{playlist}.m3u{RESET}을 업데이트 하시겠습니까? {INPUT}(y/N){RESET}: ", end="")
+				print(f"{UPDATE} {message}을 업데이트 하시겠습니까? {INPUT}(y/N){RESET}: ", end="")
 				confirm = input()
 		else:
-			print(f"{CREATE} 플레이리스트 파일 {FILE}{playlist}.m3u{RESET} 생성 중...")
+			print(f"{CREATE} {message} 생성 중...")
 			confirm = "y"
 		if confirm.strip() in ["Y", "y"]:
 			if not os.path.exists(home):
 				os.mkdir(home)
 			with open(playlist_file, "w", encoding="utf8") as m3u:
 				m3u.write("\n".join(prop["name"] for prop in prop_lst))
+		if export_to_smpl:
+			playlist_file = f"{home}/{playlist}.smpl"
+			message = f"플레이리스트 파일 {FILE}{playlist}.smpl{RESET}"
+			if os.path.exists(playlist_file):
+				if auto:
+					print(f"{CREATE} {message} 업데이트 중...")
+					confirm = "y"
+				else:
+					print(f"{UPDATE} {message}을 업데이트 하시겠습니까? {INPUT}(y/N){RESET}: ", end="")
+					confirm = input()
+			else:
+				print(f"{CREATE} {message} 생성 중...")
+				confirm = "y"
+			if confirm.strip() in ["Y", "y"]:
+				if not os.path.exists(home):
+					os.mkdir(home)
+				with open(playlist_file, "w", encoding = "utf8") as smpl:
+					smpl.write("{\"members\": [")
+					smpl.write(",".join("\n\t{\"info\": \"/storage/emulated/0/Music" + folder + "/" + prop["name"] + f"\", \"order\": {i + 1}, \"type\": 65537" + "}"
+						for i, prop in enumerate(prop_lst)))
+					smpl.write("\n], \"sortBy\": 4}")
 
 	ydl_opts["outtmpl"] = f"{home}/{playlist}" + r"/%(title)s-%(id)s.%(ext)s"
 	norm_lst = [os.path.normpath(f"{home}/{prop['name']}") for prop in prop_lst]
@@ -154,16 +182,16 @@ def download(
 				print(f"{DOWNLOAD} 파일 {FILE}{os.path.relpath(file, home)}{RESET} 다운로드 중... {INDEX}({i+1}/{total}){RESET}")
 				j = retries
 				while True:
-					if j == 0:
-						print(f"{ERROR} 파일을 다운로드할 수 없습니다.")
-						for k in glob.glob(f"{os.path.splitext(file)[0]}.*"):
-							os.remove(k)
-						break
 					try:
 						ydl.download([prop_lst[i]["url"]])
 						break
 					except:
-						print(f"{WARNING} 에러 발견. 다시 다운로드 중...")
+						if j == 0:
+							print(f"{ERROR} 파일을 다운로드할 수 없습니다.")
+							for k in glob.glob(f"{os.path.splitext(file)[0]}.*"):
+								os.remove(k)
+							break
+						print(f"{WARNING} 에러 발생. 다시 다운로드 중...")
 						ydl.cache.remove()
 						for k in glob.glob(f"{os.path.splitext(file)[0]}.*"):
 							os.remove(k)
