b0d76faaf6a12ff048bbbc446f1178a9d61f2e95
myh2910 <minleeyohan@gmail.com>
2022-02-16 09:57:23 -0500

    A lot of updates

--- playlist/__init__.py
+++ playlist/__init__.py
@@ -1,2 +1,67 @@
-from .youtube import *
-# from .utils import *
+"""
+playlist
+========
+
+Download YouTube videos and music.
+
+"""
+from __future__ import unicode_literals
+
+from timeit import default_timer
+
+import colorama
+
+from . import get
+from .config import CONFIG
+from .utils import print_time
+
+
+def download(url, ext="mp3", start=0, end=None, playlist=None, **kwargs):
+	"""
+	Download YouTube videos and music.
+
+	Parameters
+	----------
+	url : str
+		YouTube URL.
+	ext : str, optional
+		File extension ("mp3", "mp4") of the exported file.
+	start : int, optional
+		Download playlist starting from this index.
+	end : int or None, optional
+		Download playlist ending at this index.
+	playlist : str or None, optional
+		Playlist name.
+
+	Returns
+	-------
+	bool
+	"""
+	for key, value in kwargs.items():
+		CONFIG[key] = value
+
+	colorama.init()
+
+	elapsed_time = -default_timer()
+	playlist = get.extract_info(url, ext, playlist)
+	elapsed_time += default_timer()
+
+	if not playlist:
+		return False
+
+	get.__init__(ext, start, end)
+
+	if playlist != "NA":
+		if CONFIG['export_to_m3u']:
+			get.export_to_m3u()
+		if CONFIG['export_to_smpl']:
+			get.export_to_smpl()
+
+	elapsed_time -= default_timer()
+	get.download_video()
+	elapsed_time += default_timer()
+
+	get.write_diff()
+
+	print_time(elapsed_time)
+	return True
--- playlist/utils.py
+++ playlist/utils.py
@@ -1,91 +1,179 @@
-import os
+"""
+Useful functions for other modules.
+
+"""
 import glob
+import os
 import random
-from .constants import *
 
-def lst_method(lines: list[str], method: str) -> list[str]:
-	"""리스트 정렬.
+import colorama
+from colorama import Fore
 
-	옵션 설명:
-	* `lines`: 정렬할 리스트.
-	- `method`: 정렬 기준.
-		- `"suffle"`: 셔플 기능.
-		- `"name"`: 이름 순대로 정렬.
-		- `"artist"`: 아티스트 기준으로 정렬.
+from .config import CONFIG
+
+
+def colored(text, key):
 	"""
-	if method == "suffle":
-		random.shuffle(lines)
-	elif method == "name":
-		lines = sorted(lines)
-	elif method == "artist":
-		# TODO: artist 순대로 정렬
-		pass
+	Print colored text.
+
+	Parameters
+	----------
+	text : str or int
+		Text to print.
+	key : str
+		Text type.
+	"""
+	match key:
+		case "file":
+			color = Fore.LIGHTCYAN_EX
+		case "num":
+			color = Fore.LIGHTGREEN_EX
+		case "input":
+			color = Fore.LIGHTMAGENTA_EX
+		case _:
+			color = ""
+	return f"{color}{text}{Fore.RESET}"
+
+def print_time(seconds):
+	"""
+	Print elapsed time.
+
+	Parameters
+	----------
+	seconds : float
+		Time in seconds.
+
+	Returns
+	-------
+	str
+		Converted time in hours, minutes and seconds.
+	"""
+	minutes, seconds = divmod(int(seconds), 60)
+	hours, minutes = divmod(minutes, 60)
+
+	time_lst = []
+	for cnt, name in ((hours, "시간"), (minutes, "분"), (seconds, "초")):
+		time_lst.append(f'{colored(cnt, "num")}{name}')
+
+	if hours > 0:
+		idx = 0
+	elif minutes > 0:
+		idx = 1
 	else:
-		print(f"{ERROR} 옵션 {INPUT}\"{method}\"{RESET}이 존재하지 않습니다.")
-		return False
+		idx = 2
+
+	print(f":: 다운로드가 완료되었습니다. 총 {' '.join(time_lst[idx:])}가 걸렸습니다.")
+
+def sort_lines(lines, method):
+	"""
+	Sort lines.
+
+	Parameters
+	----------
+	lines : list of str
+		List of lines to sort.
+	sort_method : str
+		Sorting method ("suffle", "name", "artist").
+
+	Returns
+	-------
+	lines : list of str
+		The sorted lines.
+	"""
+	match method:
+		case "suffle":
+			random.shuffle(lines)
+		case "name":
+			lines = sorted(lines)
+		case "artist":
+			# TODO: artist 순대로 정렬
+			pass
+		case _:
+			_method = colored(f'"{method}"', "input")
+			print(f" [ERROR] 옵션 {_method}이 존재하지 않습니다.")
+			return False
 	return lines
 
-def lst_create(
-	playlist: str,
-	out: str = "m3u",
-	folder: str = "",
-	method: str = "name",
-	exts: list = ["mp3", "mp4"],
-	home: str = HOME
-) -> None:
-	"""플레이리스트 파일 생성.
-
-	옵션 설명:
-	* `playlist`:  플레이리스트 이름.
-	* `out`: 플레이리스트 파일 확장자 (`"m3u"`, `"smpl"`).
-	* `folder`: 플레이리스트를 직접적으로 포함하는 폴더 이름 (없으면 `""`).
-	* `method`: 정렬 기준.
-	* `exts`: 파일 확장자들을 포함한 리스트.
-	* `home`: 모든 플레이리스트를 포함하는 폴더 이름.
+def create_playlist(playlist, out="m3u", method="name"):
 	"""
-	home += folder
-	init()
+	Generate a playlist file.
+
+	Parameters
+	----------
+	playlist : str
+		Playlist name.
+	out : str, optional
+		Playlist file extension ("m3u", "smpl").
+	method : str, optional
+		Playlist sorting method.
+	"""
+	colorama.init()
+	print(":: 플레이리스트 생성 중...")
+
+	m3u_path = f"{CONFIG['home']}/{playlist}.m3u"
+	smpl_path = f"{CONFIG['home']}/{playlist}.smpl"
+	_playlist = colored(playlist, "file")
+
 	if out == "m3u":
-		if os.path.exists(f"{home}/{playlist}"):
-			lst = []
-			for ext in exts:
-				lst.extend(glob.glob(f"{home}/{playlist}/*.{ext}"))
-			lst = lst_method(lst, method)
-			if lst:
-				if lst == []:
-					print(f"{ERROR} 플레이리스트 폴더 {FILE}{playlist}{RESET}가 비어 있습니다.")
-				else:
-					with open(f"{home}/{playlist}.m3u", "w", encoding="utf8") as m3u:
-						for file in lst:
-							m3u.write(os.path.relpath(file, home) + "\n")
-					print(f"{FINISHED} 플레이리스트 생성이 완료되었습니다.")
-		else:
-			print(f"{ERROR} 플레이리스트 폴더 {FILE}{playlist}{RESET}를 찾을 수 없습니다.")
+		if not os.path.exists(f"{CONFIG['home']}/{playlist}"):
+			print(f" [ERROR] 플레이리스트 폴더 {_playlist}를 찾을 수 없습니다.")
+			return
+
+		lines = []
+		for ext in ["mp3", "mp4"]:
+			lines.extend(glob.glob(f"{CONFIG['home']}/{playlist}/*.{ext}"))
+
+		if not lines:
+			print(f" [ERROR] 플레이리스트 폴더 {_playlist}가 비어 있습니다.")
+			return
+
+		with open(m3u_path, "w", encoding="utf8") as m3u:
+			for file in sort_lines(lines, method):
+				m3u.write(f"{os.path.relpath(file, CONFIG['home'])}\n")
 	elif out == "smpl":
-		with open(f"{home}/{playlist}.m3u", "r", encoding = "utf8") as m3u:
+		with open(m3u_path, "r", encoding = "utf8") as m3u:
 			music = m3u.readlines()
-			with open(f"{home}/{playlist}.smpl", "w", encoding = "utf8") as smpl:
-				smpl.write("{\"members\": [")
-				for i, mp3 in enumerate(music):
-					smpl.write("\n\t{\"info\": \"/storage/emulated/0/Music" + folder + "/" + mp3 + f"\", \"order\": {i + 1}, \"type\": 65537" + "},")
-				smpl.write("\n], \"sortBy\": 4}")
+			with open(smpl_path, "w", encoding = "utf8") as smpl:
+				smpl.write('{"members": [')
+				for idx, mp3 in enumerate(music):
+					smpl.write(f"""
+\t{{"info": "/storage/emulated/0/{CONFIG['home']}/{mp3}", "order": {idx + 1}, \
+"type": 65537}},"""
+					)
+				smpl.write('\n], "sortBy": 4}')
 
-def lst_order(playlist: str, method: str = "suffle", home: str = HOME) -> None:
-	"""플레이리스트 정렬 기능.
+	print(":: 플레이리스트 생성이 완료되었습니다.")
 
-	옵션 설명:
-	* `playlist`:  플레이리스트 이름.
-	* `method`: 정렬 기준.
-	* `home`: 플레이리스트를 포함하고 있는 폴더 이름.
+def sort_playlist(playlist, method="suffle"):
 	"""
-	init()
-	if os.path.exists(f"{home}/{playlist}.m3u"):
-		with open(f"{home}/{playlist}.m3u", "r+", encoding="utf8") as m3u:
-			lines = lst_method([line.strip() for line in m3u.readlines()], method)
-			if lines:
-				m3u.seek(0)
-				m3u.writelines("\n".join(i for i in lines if i != ""))
-				m3u.truncate()
-				print(f"{FINISHED} 플레이리스트 정렬이 완료되었습니다.")
-	else:
-		print(f"{ERROR} 플레이리스트 파일 {FILE}{playlist}.m3u{RESET}을 찾을 수 없습니다.")
+	Sort a playlist file.
+
+	Parameters
+	----------
+	playlist : str
+		Playlist name.
+	method : str, optional
+		Playlist sorting method.
+	"""
+	colorama.init()
+	print(":: 플레이리스트 정렬 중...")
+
+	path = os.path.join(CONFIG['home'], f"{playlist}.m3u")
+
+	if not os.path.exists(path):
+		print(f' [ERROR] 플레이리스트 파일 {colored(f"{playlist}.m3u", "file")}을 \
+찾을 수 없습니다.')
+		return
+
+	with open(path, "r+", encoding="utf8") as file:
+		lines = sort_lines([line.strip() for line in file.readlines()], method)
+
+		if not lines:
+			print(" [ERROR] 플레이리스트가 비어 있습니다.")
+			return
+
+		file.seek(0)
+		file.writelines("\n".join(i for i in lines if i != ""))
+		file.truncate()
+
+		print(":: 플레이리스트 정렬이 완료되었습니다.")
