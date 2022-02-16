455dd1b46e141d7e04ba54bf7bd036bcf474150a
myh2910 <minleeyohan@gmail.com>
2022-02-16 17:01:08 -0500

    Add files

---
+++ playlist/config.py
@@ -0,0 +1,17 @@
+"""
+The configuration file.
+
+"""
+CONFIG = {
+	'ydl_opts': {
+		'writethumbnail': True,
+		'fragment_retries': 3,
+		'retries': 3,
+		'cachedir': False
+	},
+	'home': "Music",
+	'num_retries': 3,
+	'export_to_m3u': True,
+	'export_to_smpl': False,
+	'auto': True
+}
