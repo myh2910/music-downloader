cd14fef6bcfa4ce1983d89a3194e73be2592f86e
Yohan Min <minleeyohan@gmail.com>
2023-02-24 05:41:14 -0500

    Update

--- music_downloader/__init__.py
+++ music_downloader/__init__.py
@@ -1,18 +1,24 @@
 """
 music-downloader
-========
+================
 
 Download music and video from YouTube and more.
 
 """
-from . import get
+from . import main
 from .config import CONFIG
-from .utils import Timer
 
 __author__ = "Yohan Min"
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 
-def download(url, ext="mp3", playlist_start=0, playlist_end=None, playlist_name=None, **kwargs):
+def download(
+	url,
+	ext="mp3",
+	playlist_start=0,
+	playlist_end=None,
+	playlist_name=None,
+	**kwargs
+):
 	"""
 	Download YouTube videos and music.
 
@@ -36,29 +42,26 @@ def download(url, ext="mp3", playlist_start=0, playlist_end=None, playlist_name=
 	for key, value in kwargs.items():
 		CONFIG[key] = value
 
-	timer = Timer()
+	timer = main.Timer()
 
 	timer.start()
-	extraction_status = get.extract_info(url, ext, playlist_name)
+	extraction_status = main.extract_info(url, ext, playlist_name)
 	timer.pause()
 
 	if extraction_status == False:
 		return False
 
-	get.init(ext, playlist_start, playlist_end)
+	main.init(ext, playlist_start, playlist_end)
 
 	if extraction_status:
-		if CONFIG['write_m3u']:
-			get.export_to_m3u()
-		if CONFIG['write_smpl']:
-			get.export_to_smpl()
+		main.export_playlist(CONFIG['export_to'])
 
 	timer.start()
-	download_status = get.download_video()
+	download_status = main.download_video()
 	timer.pause()
 
 	if extraction_status and CONFIG['write_diff']:
-		get.write_diff()
+		main.write_diff()
 
 	if download_status == False:
 		return False
--- music_downloader/config.py
+++ music_downloader/config.py
@@ -11,18 +11,18 @@ video_opts : dict
 	Video options.
 auto : bool
 	Whether to skip user confirmation and automate all processes.
-write_m3u : bool
-	Whether to export playlist to an .m3u file.
-write_smpl : bool
-	Whether to export playlist to an .smpl file.
 write_diff : bool
 	Whether to write a log file.
 retries : int
 	The number of retries.
-outdir : str
-	The directory in which files will be downloaded.
 lang : str
-	Currently supports English and Korean only.
+	The language to display.
+outdir : str
+	The directory where files will be downloaded.
+smpl_outdir : str
+	The directory in a phone where Samsung Music playlist files will be located.
+export_to : list of str
+	List of playlist extensions to be exported.
 
 """
 import colorama
@@ -70,12 +70,12 @@ CONFIG = {
 		]
 	},
 	'auto': True,
-	'write_m3u': True,
-	'write_smpl': False,
 	'write_diff': True,
 	'retries': 2,
+	'lang': "ko",
 	'outdir': "Music",
-	'lang': "ko"
+	'smpl_outdir': "/storage/emulated/0/Music",
+	'export_to': ["m3u"]
 }
 
 LANG = {
@@ -89,19 +89,14 @@ LANG = {
 		'updating_file': ":: Updating playlist file %s...",
 		'generating_file': ":: Generating playlist file %s...",
 		'deleting_file': ":: Deleting file %s...",
-		'sorting_playlist': ":: Sorting playlist...",
 		'confirm_file_update': ":: Update playlist file %s? [y/N] ",
 		'confirm_file_removal': ":: Cannot find file %s from playlist. Delete? [y/N] ",
 		'file_already_exists': ":: File %s already exists. (%d/%d)",
 		'download_completed': ":: Download completed. Elapsed time: %s.",
-		'sorting_completed': ":: Playlist sorting completed.",
-		'unsupported_extension_error': "File extension %s is not supported.",
+		'invalid_extension_error': "File extension %s is not supported.",
 		'data_extraction_error': "Unable to extract URL data.",
 		'retrying_after_error': "Error occurred. Retrying...",
 		'file_download_error': "Unable to download file.",
-		'no_sort_method_error': "Option %s does not exist.",
-		'no_playlist_file_error': "Cannot find playlist file %s.",
-		'empty_playlist_error': "Playlist is empty."
 	},
 	'ko': {
 		'hours': "%d시간",
@@ -113,18 +108,13 @@ LANG = {
 		'updating_file': ":: 플레이리스트 파일 %s 업데이트 중...",
 		'generating_file': ":: 플레이리스트 파일 %s 생성 중...",
 		'deleting_file': ":: 파일 %s 삭제 중...",
-		'sorting_playlist': ":: 플레이리스트 정렬 중...",
 		'confirm_file_update': ":: 플레이리스트 파일 %s을 업데이트 하시겠습니까? [y/N] ",
 		'confirm_file_removal': ":: 파일 %s을 플레이리스트에서 찾을 수 없습니다. 지우겠습니까? [y/N] ",
 		'file_already_exists': ":: 파일 %s이 이미 존재합니다. (%d/%d)",
 		'download_completed': ":: 다운로드가 완료되었습니다. 총 %s가 걸렸습니다.",
-		'sorting_completed': ":: 플레이리스트 정렬이 완료되었습니다.",
-		'unsupported_extension_error': "파일 확장자 %s는 지원되지 않습니다.",
+		'invalid_extension_error': "파일 확장자 %s는 지원되지 않습니다.",
 		'data_extraction_error': "URL 정보 추출이 불가능합니다.",
 		'retrying_after_error': "에러 발생. 다시 시도 중...",
 		'file_download_error': "파일을 다운로드할 수 없습니다.",
-		'no_sort_method_error': "옵션 %s이 존재하지 않습니다.",
-		'no_playlist_file_error': "플레이리스트 파일 %s을 찾을 수 없습니다.",
-		'empty_playlist_error': "플레이리스트가 비어 있습니다."
 	}
 }
